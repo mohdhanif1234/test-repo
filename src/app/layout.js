215c5b1a35f55e49f783d2f25f30558867e13474
mohammad hanif <mohdhanif.topia@nw18.com>
2024-02-26 00:11:39 +0530

    initial commit2

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,20 +1,12 @@
-import Footer from "./components/Footer"
-import Navbar from "./components/Navbar"
-
-
 export const metadata = {
-  title: 'Blog',
-  description: 'I am a blog',
-}
+  title: "Blog",
+  description: "I am a blog",
+};
 
 export default function RootLayout({ children }) {
- return (
+  return (
     <html lang="en">
-      <body>
-        <Navbar/>
-        {children}
-        <Footer/>
-        </body>
+      <body>{children}</body>
     </html>
-  )
+  );
 }
